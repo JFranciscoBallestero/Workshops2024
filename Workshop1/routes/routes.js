cf82c24581f3cfbaacaaf363b1cdc835ff887906
Jose Francisco Ballestero Rivera <franciscoballesterorivera@gmail.com>
2024-05-21 04:48:42 -0600

    Update routes.js

--- Workshop1/routes/routes.js
+++ Workshop1/routes/routes.js
@@ -1,12 +1,26 @@
+
 const express = require('express');
 
 const router = express.Router()
 
+const Model = require('../model/model');
+
 module.exports = router;
 
 //Post Method
-router.post('/post', (req, res) => {
-    res.send('Post API')
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
 //Get all Method

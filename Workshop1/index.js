98273508c1b57cc4c637485aaf7deb5c33fdedef
Jose Francisco Ballestero Rivera <franciscoballesterorivera@gmail.com>
2024-05-21 04:24:48 -0600

    Update index.js

--- Workshop1/index.js
+++ Workshop1/index.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const mongoString = process.env.DATABASE_URL
+const mongoString = process.env.DATABASE_URL;
 const routes = require('./routes/routes');
 
+
 mongoose.connect(mongoString);
-const database = mongoose.connection
+const database = mongoose.connection;
 
 database.on('error', (error) => {
     console.log(error)
@@ -15,11 +16,13 @@ database.on('error', (error) => {
 database.once('connected', () => {
     console.log('Database Connected');
 })
-
 const app = express();
+
+app.use('/api', routes)
 
 app.use(express.json());
 
+
 app.listen(3000, () => {
-    console.log('Server Started at ${3000}')
+    console.log(`Server Started at ${3000}`)
 })

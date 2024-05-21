840af13f4a71e2b8e0001aeaf7224bfbe96c68bc
Jose Francisco Ballestero Rivera <franciscoballesterorivera@gmail.com>
2024-05-21 05:06:19 -0600

    Update routes.js
    Updates the Delete by ID Method

--- Workshop1/routes/routes.js
+++ Workshop1/routes/routes.js
@@ -64,6 +64,14 @@ router.patch('/update/:id', async (req, res) => {
 })
 
 //Delete by ID Method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
+//Delete by ID Method
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })

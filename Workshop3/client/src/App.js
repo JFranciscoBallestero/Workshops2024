3b74b47f0f5393037cdf1c701dd754db8b3f71f7
Jose Francisco Ballestero Rivera <franciscoballesterorivera@gmail.com>
2024-06-04 04:46:18 -0600

    created and updated client

---
+++ Workshop3/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MajorForm from './components/MajorForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>University Majors</h1>
+      </header>
+      <MajorForm />
+    </div>
+  );
+}
+
+export default App;

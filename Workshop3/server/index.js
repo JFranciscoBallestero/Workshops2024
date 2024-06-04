516218c09f0e7cfdfa991196818a001613e47fc5
Jose Francisco Ballestero Rivera <franciscoballesterorivera@gmail.com>
2024-06-04 03:51:46 -0600

    Update index.js

--- Workshop3/server/index.js
+++ Workshop3/server/index.js
@@ -1,23 +1,20 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const mongoString = process.env.DATABASE_URL;
-const routes = require('./routes/routes');
+const dotenv = require('dotenv');
+const majorRoutes = require('./routes/routes');
+
+dotenv.config();
 
 const app = express();
-
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Use the routes defined in routes.js
-app.use('/api', routes);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Could not connect to database:', err));
 
-// Connect to MongoDB
-mongoose.connect(mongoString)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+app.use('/api/majors', majorRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });

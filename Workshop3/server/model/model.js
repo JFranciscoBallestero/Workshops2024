d18a3f995767f8bc0146aa65db80854581c1a8cc
Jose Francisco Ballestero Rivera <franciscoballesterorivera@gmail.com>
2024-06-04 02:40:13 -0600

    Updates model.js

--- Workshop3/server/model/model.js
+++ Workshop3/server/model/model.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const majorSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
-    lastname: {
-        type: String,
-        required: true
+    code: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
     }
-});
-
-module.exports = mongoose.model('User', userSchema);
+  });
+  
+  module.exports = mongoose.model('Major', majorSchema);

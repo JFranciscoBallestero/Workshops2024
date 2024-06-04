4cdda5e2ee6220cd76e9ce91b4d8a58c19fd2858
Jose Francisco Ballestero Rivera <franciscoballesterorivera@gmail.com>
2024-06-04 14:08:55 -0600

    Update routes.js

--- Workshop3/server/routes/routes.js
+++ Workshop3/server/routes/routes.js
@@ -4,81 +4,82 @@ const Major = require('../model/model');
 
 // Get all majors
 router.get('/', async (req, res) => {
-    try {
-      const majors = await Major.find();
-      res.json(majors);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+  try {
+    const majors = await Major.find();
+    res.json(majors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get one major by code
+router.get('/:code', async (req, res) => {
+  try {
+    const major = await Major.findOne({ code: req.params.code });
+    if (major == null) {
+      return res.status(404).json({ message: 'Cannot find major' });
     }
+    res.json(major);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Create a major with unique code
+router.post('/', async (req, res) => {
+  const major = new Major({
+    name: req.body.name,
+    code: req.body.code,
+    description: req.body.description
   });
-  
-  // Get one major
-  router.get('/:id', async (req, res) => {
-    try {
-      const major = await Major.findById(req.params.id);
-      if (major == null) {
-        return res.status(404).json({ message: 'Cannot find major' });
-      }
-      res.json(major);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  });
-  
-  // Create a major
-  router.post('/', async (req, res) => {
-    const major = new Major({
-      name: req.body.name,
-      code: req.body.code,
-      description: req.body.description
-    });
-  
-    try {
-      const newMajor = await major.save();
-      res.status(201).json(newMajor);
-    } catch (err) {
+
+  try {
+    const newMajor = await major.save();
+    res.status(201).json(newMajor);
+  } catch (err) {
+    if (err.code === 11000) {
+      res.status(400).json({ message: 'Major with this code already exists' });
+    } else {
       res.status(400).json({ message: err.message });
     }
-  });
-  
-  // Update a major
-  router.put('/:id', async (req, res) => {
-    try {
-      const major = await Major.findById(req.params.id);
-      if (major == null) {
-        return res.status(404).json({ message: 'Cannot find major' });
-      }
-  
-      if (req.body.name != null) {
-        major.name = req.body.name;
-      }
-      if (req.body.code != null) {
-        major.code = req.body.code;
-      }
-      if (req.body.description != null) {
-        major.description = req.body.description;
-      }
-  
-      const updatedMajor = await major.save();
-      res.json(updatedMajor);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+  }
+});
+
+// Update a major by code (only update name and description)
+router.put('/:code', async (req, res) => {
+  try {
+    const major = await Major.findOne({ code: req.params.code });
+    if (major == null) {
+      return res.status(404).json({ message: 'Cannot find major' });
     }
-  });
-  
-  // Delete a major
-  router.delete('/:id', async (req, res) => {
-    try {
-      const major = await Major.findById(req.params.id);
-      if (major == null) {
-        return res.status(404).json({ message: 'Cannot find major' });
-      }
-  
-      await major.remove();
-      res.json({ message: 'Deleted major' });
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+
+    if (req.body.name != null) {
+      major.name = req.body.name;
     }
-  });
-  
-  module.exports = router;
+    if (req.body.description != null) {
+      major.description = req.body.description;
+    }
+
+    const updatedMajor = await major.save();
+    res.json(updatedMajor);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete a major by code
+router.delete('/:code', async (req, res) => {
+  try {
+    const major = await Major.findOne({ code: req.params.code });
+    if (major == null) {
+      return res.status(404).json({ message: 'Cannot find major' });
+    }
+
+    await major.remove();
+    res.json({ message: 'Deleted major' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
